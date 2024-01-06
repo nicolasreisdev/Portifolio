b6a9c328921af5a80a27b188b46efd992945d197
Nicolas Reis <nicolas.e.silva@ufv.br>
2024-01-06 20:57:47 -0300

    Alterações de entrada

--- click.js
+++ click.js
@@ -1,62 +1,67 @@
-var b = window.document.getElementById('prof');
-var hard = window.document.getElementById('hard');
+var profisional = window.document.getElementById('prof');
+/*var hard = window.document.getElementById('hard');
 var soft = window.document.getElementById('soft');
-var cont = window.document.getElementById('contatos');
-var a = window.document.getElementById('pessoal');
+var cont = window.document.getElementById('contatos');*/
+var pessoal = window.document.getElementById('pessoal');
 function clicar(){
     window.location = "pessoal.html";
 }
 function entrar(){
-    a.style.backgroundColor = 'transparent';
-    a.style.color = 'white';
-    a.style.border='1px solid blue';
+    pessoal.style.backgroundColor = 'transparent';
+    pessoal.style.color = 'white';
+    pessoal.style.border='1px solid blue';
 }
 function sair(){
-    a.style.backgroundColor = 'transparent';
-    a.style.color = 'white';
-    a.style.border = '1px solid white';
+    pessoal.style.backgroundColor = 'transparent';
+    pessoal.style.color = 'white';
+    pessoal.style.border = '1px solid white';
 }
 function clicar2(){
     window.location = "profissional.html";
 }
 function entrar2(){
-    b.style.backgroundColor = 'transparent';
-    b.style.color = 'white';
-    b.style.border ='1px solid green';
+    profisional.style.backgroundColor = 'transparent';
+    profisional.style.color = 'white';
+    profisional.style.border ='1px solid green';
 }
 function sair2(){
-    b.style.backgroundColor = 'transparent';
-    b.style.color = 'white';
-    b.style.border = ' 1px solid white';
-}
-function dic2(){
-    hard.style.backgroundColor = 'transparent';
-    hard.style.color = 'black';
-    hard.style.border = '1px solid blue'
-}
-function dic(){
-    soft.style.backgroundColor = 'transparent';
-    soft.style.color = 'black';
-    soft.style.border = '1px solid black'
-}
-function dic3(){
-    cont.style.backgroundColor = 'transparent';
-    cont.style.color = 'black';
-    cont.style.border = '1px solid blue'
-}
-function exit(){
-    soft.style.backgroundColor = 'transparent';
-    soft.style.color = 'black';
-    soft.style.border = '1px solid black';
-}
-function exit2(){
-    hard.style.backgroundColor = 'transparent';
-    hard.style.color = 'white';
-    hard.style.border = '1px solid white';
-}
-function exit3(){
-    cont.style.backgroundColor = 'transparent';
-    cont.style.color = 'white';
-    cont.style.border = '1px solid white';
+    profisional.style.backgroundColor = 'transparent';
+    profisional.style.color = 'white';
+    profisional.style.border = ' 1px solid white';
 }
     
+/*function enter(){
+    if(hard){
+        hard.style.backgroundColor = 'transparent';
+        hard.style.color = 'white';
+        hard.style.border = '1px solid blue';
+    }
+    else if(cont){
+        cont.style.backgroundColor = 'transparent';
+        cont.style.color = 'white';
+        cont.style.border = '1px solid blue';
+    }
+    else if(soft){
+        soft.style.backgroundColor = 'transparent';
+        soft.style.color = 'black';
+        soft.style.border = '1px solid blue';
+    }
+}
+
+function exit(){    
+    if(hard){
+        hard.style.backgroundColor = 'transparent';
+        hard.style.color = 'white';
+        hard.style.border = '1px solid white';
+    }
+    else if(cont){
+        cont.style.backgroundColor = 'transparent';
+        cont.style.color = 'white';
+        cont.style.border = '1px solid white';
+    }
+    else if(soft){
+        soft.style.backgroundColor = 'transparent';
+        soft.style.color = 'black';
+        soft.style.border = '1px solid black';
+    }
+}*/
